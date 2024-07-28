567817de2328ad44dc1b65583d914be1a73f0c8a
maulwurfDE <me+github@hofmannmoritz.com>
2024-07-28 22:11:09 +0200

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const element = document.getElementById('typewriter');
-    const words = ['Founders.', 'CTOs.', 'Self-Employed.', 'CEOs.'];
+    const words = ['Founders.', 'Hustlers.', 'Dreamers.', 'Hackers.'];
     let currentWord = 0;
     let currentLetter = 0;
     let isDeleting = false;
